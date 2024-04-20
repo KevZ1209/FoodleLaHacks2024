75b05044f169a45eef1e39fc4be57fa63ff99391
marcuscheng123 <marcusdcheng@gmail.com>
2024-04-20 12:16:35 -0700

    Nutrition label

--- src/lib/components/NutritionalLabel.svelte
+++ src/lib/components/NutritionalLabel.svelte
@@ -1,20 +1,81 @@
 <script>
     export let nutritionData;
 </script>
-
-<p>Serving Size: {nutritionData.size}g</p>
-<p>Calories: {nutritionData.calories}</p>
-<p>Protein: {nutritionData.protein}g</p>
-<p>Carbs: {nutritionData.carbs}g</p>
-<p>Fat: {nutritionData.fat}g</p>
-<p>Saturated Fat: {nutritionData.sat_fat}g</p>
-<p>Trans Fat: {nutritionData.trans_fat}g</p>
-<p>Cholesterol: {nutritionData.cholesterol}mg</p>
-<p>Fiber: {nutritionData.fiber}g</p>
-<p>Sugar: {nutritionData.sugar}g</p>
-<p>Added Sugar: {nutritionData.added_sugar}g</p>
-<p>Vitamin D: {nutritionData.vitamin_d}%</p>  
-<p>Calcium: {nutritionData.calcium}mg</p>
-<p>Iron: {nutritionData.iron}mg</p>
-<p>Potassium: {nutritionData.potassium}mg</p>
-<p>Sodium: {nutritionData.sodium}mg</p>
+<div class="border-2 border-black mt-4">
+    <div class="grid grid-cols-3 gap-4 text-xl text-center mb-2">
+        <div>
+            <p class="text-4xl">{nutritionData.size}g</p>
+            <p>Serving Size</p>
+        </div>
+        <div>
+            <p class="text-4xl">{nutritionData.calories} Cal.</p>
+            <p>Calories</p>
+        </div>
+        <div>
+            <p class="text-4xl">{nutritionData.sodium}mg</p>
+            <p>Sodium</p>
+        </div>
+    </div>
+    <div class="grid grid-cols-3 gap-4 text-xl text-center mb-2">
+        <div>
+            <p class="text-4xl">{nutritionData.fat}g</p>
+            <p>Fat</p>
+        </div>
+        <div>
+            <p class="text-4xl">{nutritionData.protein}g</p>
+            <p>Protein</p>
+        </div>
+        <div>
+            <p class="text-4xl">{nutritionData.carbs}g</p>
+            <p>Carbs</p>
+        </div>
+    </div>
+    <div class="grid grid-cols-3 gap-4 text-md text-center">
+        <div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Saturated Fat:</p>
+                <p>{nutritionData.sat_fat}g</p>
+            </div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Trans Fat:</p>
+                <p>{nutritionData.trans_fat}g</p>
+            </div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Cholesterol:</p>
+                <p>{nutritionData.cholesterol}mg</p>
+            </div>
+        </div>
+        <div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Potassium:</p>
+                <p>{nutritionData.potassium}mg</p>
+            </div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Fiber:</p>
+                <p>{nutritionData.iron}mg</p>
+            </div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Calcium:</p>
+                <p>{nutritionData.calcium}mg</p>
+            </div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Vitamin D:</p>
+                <p>{nutritionData.vitamin_d}mg</p>
+            </div>
+        </div>
+        <div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Fiber:</p>
+                <p>{nutritionData.fiber}g</p>
+            </div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Sugar:</p>
+                <p>{nutritionData.sugar}g</p>
+            </div>
+            <div class="flex justify-between items-center ps-4 pe-4">
+                <p class="mr-2">Added Sugar:</p>
+                <p>{nutritionData.added_sugar}g</p>
+            </div>
+        </div>
+    </div>
+</div>
