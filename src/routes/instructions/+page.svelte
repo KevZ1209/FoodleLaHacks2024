312f967a5c71639990409bf2172634687655498a
marcuscheng123 <marcusdcheng@gmail.com>
2024-04-20 11:19:25 -0700

    instructions page

---
+++ src/routes/instructions/+page.svelte
@@ -0,0 +1,15 @@
+<div class="h-6"></div>
+<div class="bg-white max-w-2xl mx-auto text-black p-8 pt-2 jersey-10-regular border-4 border-black">
+    <div class="border-b-2 p-2 border-dashed m-2 border-black text-center">
+        <h2 class="text-4xl">How to Play</h2>
+    </div>
+    <div class="ps-5">
+        <ol class="list-decimal text-lg">
+            <li>Try to guess the food in 6 guesses</li>
+            <li>Box color for each category indicates the closeness of your guess</li>
+            <li>Hints are given out after every guess</li>
+        </ol>    
+    </div>
+</div>
+<a href="/"><button>Home</button></a>
+<div class="h-6"></div>
