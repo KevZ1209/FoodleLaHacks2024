d116cc6cdbb2526dab7abf231233de8e02b8570f
marcuscheng123 <marcusdcheng@gmail.com>
2024-04-20 23:04:01 -0700

    sdklf

--- src/lib/components/Arrow.svelte
+++ src/lib/components/Arrow.svelte
@@ -4,12 +4,12 @@
     export let real;
 </script>
 
-{#if parseFloat(guess) + 0.1 >= parseFloat(real) && parseFloat(guess) - 0.1 <= parseFloat(real)}
-    <Check size={16} class="inline" color="green"/>
+{#if parseFloat(guess) > parseFloat(real)}
+	<ArrowDown size={16} class="inline" color="red"/>
 {:else if parseFloat(guess) < parseFloat(real)}
     <ArrowUp size={16} class="inline" color="blue"/>
-{:else if parseFloat(guess) > parseFloat(real)}
-    <ArrowDown size={16} class="inline" color="red"/>
+{:else if parseFloat(guess) == parseFloat(real)}
+    <Check size={16} class="inline" color="green"/>
 {/if}
 
 
